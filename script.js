6143b653e95cc680f34037a6ca92f8a4ce7bb1f0
arthvm <90999445+arthvm@users.noreply.github.com>
2024-03-06 20:27:53 -0300

    Change .then to async/await function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const videosContainer = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-  .then(
-    (res) => res.json() // DEBUG PURPOSE
-  )
-  .then((videos) =>
-    videos.forEach((video) => {
-      videosContainer.innerHTML += `
+async function fetchAndShowVideos() {
+  const fetchVideos = await fetch("http://localhost:3000/videos");
+  const videos = await fetchVideos.json();
+
+  videos.forEach((video) => {
+    videosContainer.innerHTML += `
           <li class="videos__item">
               <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
               <div class="descricao-video">
@@ -16,10 +15,7 @@ const api = fetch("http://localhost:3000/videos")
               </div>
           </li>
       `;
-    })
-  )
-  .catch((error) => {
-    videosContainer.innerHTML = `
-        <p>Houve um erro ao carregar os videos: ${error}</p>
-    `;
   });
+}
+
+fetchAndShowVideos();
