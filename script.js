1bbde5b8fb0464e66a5661e37a24e0744c731540
arthvm <90999445+arthvm@users.noreply.github.com>
2024-03-06 20:33:54 -0300

    Add try/catch to the function that fetchs and show the video

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const videosContainer = document.querySelector(".videos__container");
 
 async function fetchAndShowVideos() {
-  const fetchVideos = await fetch("http://localhost:3000/videos");
-  const videos = await fetchVideos.json();
+  try {
+    const fetchVideos = await fetch("http://localhost:3000/videos");
+    const videos = await fetchVideos.json();
 
-  videos.forEach((video) => {
-    videosContainer.innerHTML += `
+    videos.forEach((video) => {
+      videosContainer.innerHTML += `
           <li class="videos__item">
               <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
               <div class="descricao-video">
@@ -15,7 +16,10 @@ async function fetchAndShowVideos() {
               </div>
           </li>
       `;
-  });
+    });
+  } catch (error) {
+    videosContainer.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`;
+  }
 }
 
 fetchAndShowVideos();
